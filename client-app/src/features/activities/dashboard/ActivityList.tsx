a2391dcbca2ddbaf3f2580a339f49735864f9f21
macpeluso <macker1993@gmail.com>
2019-09-13 17:47:26 -0400

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,18 +1,15 @@
-import React, { SyntheticEvent } from "react";
+import React, { useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
-interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    submitting: boolean;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-    target: string;
-}
-const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActivity, submitting, target }) => {
+import { observer } from "mobx-react-lite";
+import ActivityStore from "../../../app/stores/activityStore";
+
+const ActivityList: React.FC = () => {
+    const activityStore = useContext(ActivityStore);
+    const { activitiesByDate, selectActivity, deleteActivity, submitting, target } = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as="a">{activity.title}</Item.Header>
@@ -43,4 +40,4 @@ const ActivityList: React.FC<IProps> = ({ activities, selectActivity, deleteActi
     );
 };
 
-export default ActivityList;
+export default observer(ActivityList);
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,17 +1,17 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "./../../../app/models/activity";
 import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 interface IProps {
-    setEditMode: (editMode: boolean) => void;
     activity: IActivity;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean;
 }
-const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: iniitialFormState, createActivity, editActivity, submitting }) => {
+const ActivityForm: React.FC<IProps> = ({ activity: initialFormState }) => {
+    const activityStore = useContext(ActivityStore);
+    const { createActivity, editActivity, submitting, cancelFormOpen } = activityStore;
     const initializeForm = () => {
-        if (iniitialFormState) return iniitialFormState;
+        if (initialFormState) return initialFormState;
         else
             return {
                 id: "",
@@ -42,16 +42,28 @@ const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: iniitialFormSta
         <Segment clearing>
             <Form onSubmit={handleSubmit}>
                 <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
-                <Form.TextArea rows={2} placeholder="Description" name="description" value={activity.description} onChange={handleInputChange} />
+                <Form.TextArea
+                    rows={2}
+                    placeholder="Description"
+                    name="description"
+                    value={activity.description}
+                    onChange={handleInputChange}
+                />
                 <Form.Input placeholder="Category" name="category" value={activity.category} onChange={handleInputChange} />
-                <Form.Input type="datetime-local" placeholder="Date" name="date" value={activity.date} onChange={handleInputChange} />
+                <Form.Input
+                    type="datetime-local"
+                    placeholder="Date"
+                    name="date"
+                    value={activity.date}
+                    onChange={handleInputChange}
+                />
                 <Form.Input placeholder="City" name="city" onChange={handleInputChange} value={activity.city} />
                 <Form.Input placeholder="Venue" name="venue" onChange={handleInputChange} value={activity.venue} />
                 <Button floated="right" positive type="submit" content="Submit" loading={submitting} />
-                <Button onClick={() => setEditMode(false)} floated="right" type="button" content="Cancel" />
+                <Button onClick={cancelFormOpen} floated="right" type="button" content="Cancel" />
             </Form>
         </Segment>
     );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
