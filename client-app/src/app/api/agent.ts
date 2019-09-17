b047e97b8076665d69983b9dcaf9c20bda793ffa
macpeluso <macker1993@gmail.com>
2019-09-17 12:06:27 -0400

    End of Section 10

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,11 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 import { IActivity } from "./../models/activity";
+import { history } from "../..";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+axios.interceptors.response.use(undefined, error => {
+    if (error.message === "Network Error" && !error.response) toast.error("Network Error");
+
+    const { status, data, config } = error.response;
+
+    if (status === 404) history.push("/notfound");
+    if (status === 400 && config.method === "get" && data.errors.hasOwnProperty("id")) history.push("/notfound");
+    if (status === 500) toast.error("Server error - check the terminal for more info!");
+});
+
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) => new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+const sleep = (ms: number) => (response: AxiosResponse) =>
+    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
 
 const requests = {
     get: (url: string) =>
