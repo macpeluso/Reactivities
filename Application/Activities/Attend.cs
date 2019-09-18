34b7d7a9b842c138a5a14d71c3dcfa46ad48551a
macpeluso <macker1993@gmail.com>
2019-09-18 16:32:47 -0400

    End of Section 14

---
+++ Application/Activities/Attend.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Attend
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext context;
+            private readonly IUserAccessor userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                this.userAccessor = userAccessor;
+                this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities.FindAsync(request.Id);
+
+                if (activity == null) throw new RestException(HttpStatusCode.NotFound, new { Activity = "Could not find activity" });
+
+                var user = await context.Users.SingleOrDefaultAsync(x => x.UserName == userAccessor.GetCurrentUsername());
+
+                var attendance = await context.UserActivities.SingleOrDefaultAsync(x => x.ActivityId == activity.Id && x.AppUserId == user.Id);
+
+                if (attendance != null) throw new RestException(HttpStatusCode.BadRequest, new { Attendance = "Already attending this activity" });
+
+                attendance = new UserActivity
+                {
+                    Activity = activity,
+                    AppUser = user,
+                    IsHost = false,
+                    DateJoined = DateTime.Now
+                };
+
+                context.UserActivities.Add(attendance);
+
+                var success = await context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new System.Exception("Problem saving changes.");
+            }
+        }
+    }
+}
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -2,9 +2,11 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -38,9 +40,11 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext context;
+            private readonly IUserAccessor userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                this.userAccessor = userAccessor;
                 this.context = context;
             }
 
@@ -58,6 +62,17 @@ namespace Application.Activities
                 };
 
                 context.Activities.Add(activity);
+
+                var user = await context.Users.SingleOrDefaultAsync(u=>u.UserName == userAccessor.GetCurrentUsername());
+                
+                var attendee = new UserActivity{
+                    AppUser=user,
+                    Activity=activity,
+                    IsHost=true,
+                    DateJoined=DateTime.Now
+                };
+
+                context.UserActivities.Add(attendee);
 
                 var success = await context.SaveChangesAsync() > 0;
 
